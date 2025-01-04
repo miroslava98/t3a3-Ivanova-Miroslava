babf46d0575060eefc4e6e2f9e4c7ba1bde411e1
miroslava <miv986@hotmail.com>
2025-01-04 17:44:26 +0100

    Ejercicio 1 T7 - en proceso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.t3a3_ivanova_miroslava"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.t3a3_ivanova_miroslava"
@@ -49,4 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- app/src/main/java/com/example/t3a3_ivanova_miroslava/activities/SaludoActivity.kt
+++ app/src/main/java/com/example/t3a3_ivanova_miroslava/activities/SaludoActivity.kt
@@ -2,28 +2,49 @@ package com.example.t3a3_ivanova_miroslava.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.t3a3_ivanova_miroslava.R
 import com.example.t3a3_ivanova_miroslava.bd.MiBancoOperacional
 import com.example.t3a3_ivanova_miroslava.databinding.ActivitySaludoBinding
 import com.example.t3a3_ivanova_miroslava.pojo.Cliente
 import com.example.t3a3_ivanova_miroslava.pojo.Cuenta
+import com.google.android.material.navigation.NavigationView
 
-class SaludoActivity : AppCompatActivity() {
+class SaludoActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
 
     private lateinit var binding: ActivitySaludoBinding
     private var cliente: Cliente? = null
     private var cuenta: Cuenta? = null
+    private lateinit var drawerLayout: DrawerLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySaludoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.main)
+
+        val toolbar = findViewById<Toolbar>(R.id.bottomAppBar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
 
         cliente = intent.getSerializableExtra("Cliente") as Cliente
 
@@ -45,8 +66,7 @@ class SaludoActivity : AppCompatActivity() {
             }
         }
 
-        //BOTÓN CONTRASEÑA
-
+        //BOTÓN CONTRASEÑA ̣
         binding.btnContrasenya.setOnClickListener {
             if (cliente != null) {
                 val intent = Intent(this, PasswordActivity::class.java)
@@ -90,5 +110,9 @@ class SaludoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
